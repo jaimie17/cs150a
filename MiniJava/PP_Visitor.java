b49e3cd33afb2a50ecda9a0eb9308cd24f8a4696
jaimie17 <jaimielouie@brandeis.edu>
2024-03-20 21:50:11 -0400

    updated files

--- MiniJava/PP_Visitor.java
+++ MiniJava/PP_Visitor.java
@@ -98,7 +98,7 @@ import syntaxtree.*;
 
    public Object visit(BooleanType node, Object data){
         int indent = (int) data; 
-        return "bool";
+        return "boolean";
    }
 
    public Object visit(IdentifierType node, Object data){
@@ -136,9 +136,9 @@ import syntaxtree.*;
         String expr = (String)node.e.accept(this,indent+1);
         String s = (String) node.s.accept(this,indent+1);
         return indentString(indent)+ 
-             "while ("+expr+")"+
+             "while ("+expr+")\n"+
              s+
-             indentString(indent)+"}\n";
+             "\n";
    }
 
    public Object visit(Print node, Object data){
@@ -214,11 +214,11 @@ import syntaxtree.*;
 
    public Object visit(Call node, Object data){
         int indent = (int) data; 
-        //String e1 = (String) node.e1.accept(this,indent);
+        String e1 = (String) node.e1.accept(this,indent);
         String i = (String) node.i.accept(this,indent);
         String e2 = (String) node.e2.accept(this,indent);
 
-        return i+"("+e2+")";
+        return e1+"."+i+"("+e2+")";
    }
 
    public Object visit(IntegerLiteral node, Object data){
